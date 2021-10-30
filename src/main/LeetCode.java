86622f95c9c1cfe4a489a413b82749b297338ff4
xiaoyegu <xiaoyege7@Gmail.com>
2021-10-30 09:28:00 +0800

    提交

--- src/main/LeetCode.java
+++ src/main/LeetCode.java
@@ -26,5 +26,18 @@ public class LeetCode {
         }
         return ans;
     }
-
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left<=right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
