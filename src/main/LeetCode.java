f6921524d08a71460c7bcee92525b62cd24c97b8
xiaoyegu <xiaoyege7@Gmail.com>
2021-10-31 11:35:54 +0800

    提交10.31

--- src/main/LeetCode.java
+++ src/main/LeetCode.java
@@ -48,5 +48,39 @@ public class LeetCode {
     }
     //28. 实现 strStr()
 
+    //977. 有序数组的平方（双指针）
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int negative = -1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < 0) {
+                negative = i;
+            } else {
+                break;
+            }
+        }
+
+        int[] ans = new int[n];
+        int index = 0, i = negative, j = negative + 1;
+        while (i >= 0 || j < n) {
+            if (i < 0) {
+                ans[index] = nums[j] * nums[j];
+                ++j;
+            } else if (j == n) {
+                ans[index] = nums[i] * nums[i];
+                --i;
+            } else if (nums[i] * nums[i] < nums[j] * nums[j]) {
+                ans[index] = nums[i] * nums[i];
+                --i;
+            } else {
+                ans[index] = nums[j] * nums[j];
+                ++j;
+            }
+            ++index;
+        }
+
+        return ans;
+    }
+
 
 }
--- src/main/demo0614/Test01.java
+++ src/main/demo0614/Test01.java
@@ -1,22 +1,31 @@
 package main.demo0614;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Test01 {
+    Map<Integer, Integer> memo = new HashMap<Integer, Integer>();
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Scanner in1 = new Scanner(in.nextLine());
-        int[] nums = new int[10];
-        for (int i = 0; in1.hasNext(); i++) {
-            nums[i] = in1.nextInt();
+        String s = in.next();
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
         }
-        System.out.println(Arrays.toString(nums));
-        int[] ans = new int[nums.length];
-        for (int i = 0; i < nums.length; ++i) {
-            ans[i] = nums[i] * nums[i];
-        }
-        Arrays.sort(ans);
-        System.out.println(Arrays.toString(ans));
+        System.out.print(ans);
     }
+
 }
